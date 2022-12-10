0a49399aadcc034a72193b63e68a0f8c15be8f70
Nisa Agni <nisa.agniafifah20@gmail.com>
2022-12-11 01:48:44 +0700

    adding page perilisan

--- src/scripts/views/pages/perilisan.js
+++ src/scripts/views/pages/perilisan.js
@@ -1,3 +1,18 @@
+import { initializeApp } from 'firebase/app';
+// eslint-disable-next-line import/no-unresolved
+import swal from 'sweetalert';
+import {
+  getFirestore,
+  collection,
+  doc,
+  query,
+  getDoc,
+  deleteDoc,
+  getDocs,
+  updateDoc,
+} from 'firebase/firestore';
+import firebaseConfig from '../../globals/firebase-config';
+
 const PerilisanPage = {
   async render() {
     return `
@@ -28,7 +43,7 @@ const PerilisanPage = {
             <th scope="col">Aksi</th>
             </tr>
         </thead>
-        <tbody id="tabel_periisan"></tbody>
+        <tbody id="tabel_perilisan"></tbody>
         </table>
         </div>
       </div>
@@ -49,19 +64,25 @@ const PerilisanPage = {
               <div class="row row-cols-2">
                 <div class="col">
                   <div class="mb-3">
-                  <label for="exampleInputDate" class="form-label">Tanggal Peneluran</label>
-                  <input type="date" class="form-control" id="exampleInputDate" aria-describedby="dateHelp">
+                  <label for="exampleInputDate" class="form-label">Tanggal Perilisan</label>
+                  <input type="date" class="form-control" id="update-tglPerilisan" aria-describedby="dateHelp">
                 </div>
                 </div>
                 <div class="col">
                   <div class="mb-3">
-                  <label for="exampleInputWeather" class="form-label">Cuaca</label>
+                  <label for="exampleInputDate" class="form-label">Waktu Perilisan</label>
+                  <input type="time" class="form-control" id="update-waktuPerilisan" aria-describedby="dateHelp">
+                </div>
+                </div> 
+                <div class="col">
+                  <div class="mb-3">
+                  <label for="exampleInputWeather" class="form-la bel">Cuaca</label>
                 <div class="input-group mb-3">
-                  <select class="form-select" id="inputGroupSelect01">
+                  <select class="form-select" id="update-cuaca">
                     <option selected>Kondisi Cuaca...</option>
-                    <option value="1">Cerah</option>
-                    <option value="2">Mendung</option>
-                    <option value="3">Hujan</option>
+                    <option value="Cerah">Cerah</option>
+                    <option value="Mendung">Mendung</option>
+                    <option value="Hujan">Hujan</option>
                   </select>
                 </div>
                 </div>
@@ -75,10 +96,10 @@ const PerilisanPage = {
                   <div class="mb-3">
                     <label for="inputJenisPenyu" class="form-label">Jenis Penyu</label>
                 <div class="input-group mb-3">
-                  <select class="form-select" id="inputJenisPenyu">
+                  <select class="form-select" id="update-JenisPenyu">
                     <option selected>Jenis Penyu</option>
-                    <option value="1">Lekang</option>
-                    <option value="2">Sisik</option>
+                    <option value="Lekang">Lekang</option>
+                    <option value="Sisik">Sisik</option>
                   </select>
                 </div>
                   </div>
@@ -86,7 +107,7 @@ const PerilisanPage = {
                 <div class="col">
                   <div class="mb-3">
                   <label for="jumlahTukikRilis" class="form-label">Jumlah Tukik </label>
-                  <input type="number" class="form-control" id="jumlahTukikRilis" aria-describedby="dateHelp" placeholder="ekor">
+                  <input type="number" class="form-control" id="update-jumlahTukikRilis" aria-describedby="dateHelp" placeholder="ekor">
                 </div>
             </div>
                 </div>
@@ -94,8 +115,8 @@ const PerilisanPage = {
               </form>
       </div>
             <div class="modal-footer mx-auto">
-              <button type="button" class="btn btn-outline-primary" data-bs-dismiss="modal">Batal</button>
-              <button type="button" class="btn btn-primary">Simpan</button>
+            <button type="button" class="btn ghost-button" data-bs-dismiss="modal">Batal</button>
+            <button type="button" class="btn filled-button updateBtn" data-bs-dismiss="modal">Update</button>
             </div>
           </div>
         </div>
@@ -104,8 +125,106 @@ const PerilisanPage = {
   },
 
   async afterRender() {
-    // FUngsi dipanggil setelah render()
+    const RENDER_EVENT = 'render-event';
+    const app = initializeApp(firebaseConfig);
+    const database = getFirestore(app);
+    const tableContainer = document.getElementById('tabel_perilisan');
+    // Delete Data (menghapus data sarang)
+    const deleteRilis = async (id) => {
+      const text = 'Yakin ingin menghapus data?';
+      if (confirm(text) === true) {
+        await deleteDoc(doc(database, 'rilis', id));
+        console.log('deleteing is succeess');
+        swal('Berhasil Menghapus data', '', 'success');
+      }
+
+      document.dispatchEvent(new Event(RENDER_EVENT));
+    };
+
+    // Edit Data (mengubah data sarang)
+    const updateRilis = async (id) => {
+      const updatetglPerilisan = document.getElementById('update-tglPerilisan');
+      const updatewaktuPerilisan = document.getElementById('update-waktuPerilisan');
+      const updateCuaca = document.getElementById('update-cuaca');
+      const updatejenisPenyu = document.getElementById('update-JenisPenyu');
+      const updatejumlahTukikRilis = document.getElementById('update-jumlahTukikRilis');
+
+      const docSnap = await getDoc(doc(database, 'rilis', id));
+      if (docSnap.exists()) {
+        updatetglPerilisan.value = docSnap.data().tglPerilisan;
+        updatewaktuPerilisan.value = docSnap.data().waktuPerilisan;
+        updateCuaca.value = docSnap.data().cuaca;
+        updatejenisPenyu.value = docSnap.data().jenisPenyu;
+        updatejumlahTukikRilis.value = docSnap.data().jumlahTukikRilis;
+
+        const confirmUpdatingRilisButton = document.querySelector('.updateBtn');
+        confirmUpdatingRilisButton.addEventListener('click', async () => {
+          // eslint-disable-next-line no-shadow
+          const updateRilis = {
+            tglPerilisan: updatetglPerilisan.value,
+            waktuPerilisan: updatewaktuPerilisan.value,
+            cuaca: updateCuaca.value,
+            jenisPenyu: updatejenisPenyu.value,
+            jumlahTukikRilis: updatejumlahTukikRilis.value,
+          };
+
+          await updateDoc(doc(database, 'rilis', id), updateRilis);
+          document.dispatchEvent(new Event(RENDER_EVENT));
+          swal('Sukses', 'Data berhasil diupdate', 'success');
+        });
+        confirmUpdatingRilisButton.setAttribute('id', id);
+      } else {
+        console.log('No such document!');
+      }
+    };
+
+    document.addEventListener(RENDER_EVENT, async () => {
+      tableContainer.innerHTML = '';
+      const q = query(collection(database, 'rilis'));
+
+      const querySnapshot = await getDocs(q);
+      let index = 1;
+      querySnapshot.forEach((item) => {
+        // console.log(item);
+        const sarangElement = document.createElement('tr');
+        sarangElement.innerHTML += `
+        <th scope="row" class="text-center">${index}</th>
+        <td class="text-center">${item.data().tglPerilisan}</td>
+        <td class="text-center">${item.data().waktuPerilisan}</td>
+        <td class="text-center">${item.data().cuaca}</td>
+        <td class="text-center">${item.data().jenisPenyu}</td>
+        <td class="text-center">${item.data().jumlahTukikRilis}</td>
+        <td class="text-center">
+        <button type="button" class="btn btn-warning px-2 rounded-4 update-btn" data-bs-toggle="modal" data-bs-target="#ubahRilisModal" title="Ubah Data" id=${item.id}><i class="bi bi-pencil-square action-btn"></i></button>
+        <button type="button" class="btn btn-danger px-2 rounded-4 delete-btn" title="Hapus Data" id=${item.id}><i class="bi bi-trash-fill"></i></button>
+        </td>
+        `;
+
+        tableContainer.appendChild(sarangElement);
+
+        index += 1;
+      });
+      const deleteRilisButtons = document.querySelectorAll('.delete-btn');
+      deleteRilisButtons.forEach((deleteButton) => {
+        deleteButton.addEventListener('click', (event) => {
+          event.stopPropagation();
+          const sarangId = event.currentTarget.id;
+          console.log(sarangId);
+
+          deleteRilis(sarangId);
+        });
+      });
+      const updateRilisButtons = document.querySelectorAll('.update-btn');
+      updateRilisButtons.forEach((updateButton) => {
+        updateButton.addEventListener('click', (event) => {
+          event.stopPropagation();
+          const sarangId = event.currentTarget.id;
+          updateRilis(sarangId);
+        });
+      });
+    });
+
+    document.dispatchEvent(new Event(RENDER_EVENT));
   },
 };
-
 export default PerilisanPage;
